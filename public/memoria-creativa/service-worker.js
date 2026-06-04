7ff8d54df3d33ee27b251a91173016d244055de4
sandysarmiento-max <sandy.sarmiento@gmail.com>
2026-06-04 10:35:03 -0500

    Restringir limpieza de cache a Memoria Creativa

--- public/memoria-creativa/service-worker.js
+++ public/memoria-creativa/service-worker.js
@@ -36,7 +36,13 @@ self.addEventListener("install", (event) => {
 self.addEventListener("activate", (event) => {
   event.waitUntil(
     caches.keys()
-      .then((keys) => Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))))
+            .then((keys) =>
+        Promise.all(
+          keys
+            .filter((key) => key.startsWith("memoria-creativa-") && key !== CACHE_NAME)
+            .map((key) => caches.delete(key))
+        )
+      )
       .then(() => self.clients.claim())
   );
 });
